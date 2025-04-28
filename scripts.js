7f39d3d6a775e3714af4b38b25a21fe1756dfdae
Samir <samirjafari2001@icloud.com>
2025-04-28 11:02:35 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+});
