af3889ff66c106dc9009e7fa0da0a3f1086fe1e4
Samir <samirjafari2001@icloud.com>
2025-04-29 00:04:03 +0300

    Пакетные туры

--- swiperScript.js
+++ swiperScript.js
@@ -1,9 +1,34 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".author_tours_swiper", {
   slidesPerView: 1, // базовое значение (для мобильных)
   spaceBetween: 30, // базовый отступ
   freeMode: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".author_tours-swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+  breakpoints: {
+    430: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
+
+var swiper = new Swiper(".package_tours_swiper", {
+  slidesPerView: 1, // базовое значение (для мобильных)
+  spaceBetween: 30, // базовый отступ
+  freeMode: true,
+  pagination: {
+    el: ".package_tours-swiper-pagination",
     clickable: true,
     type: "bullets",
   },
